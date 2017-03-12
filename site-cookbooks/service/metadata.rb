a47a53da8784cf2f1b40bd165434541df827677d
krowpu <krowpu@tightmail.com>
2017-03-12 18:54:04 +0500

    Create cookbook "service"

---
+++ site-cookbooks/service/metadata.rb
@@ -0,0 +1,7 @@
+name 'service'
+description 'Capybara Webkit daemon systemd service'
+license 'MIT'
+
+platform 'ubuntu', '= 16.04'
+
+depends 'deploy'
---
+++ site-cookbooks/service/recipes/default.rb
@@ -0,0 +1,32 @@
+include_recipe 'deploy'
+
+directory '/srv/scrapod/shared' do
+  owner node['deploy_user']
+  group node['deploy_user']
+  mode '0755' # drwxr-xr-x
+end
+
+directory '/srv/scrapod/shared/tmp' do
+  owner node['deploy_user']
+  group node['deploy_user']
+  mode '0755' # drwxr-xr-x
+end
+
+directory '/srv/scrapod/shared/tmp/pids' do
+  owner node['deploy_user']
+  group node['deploy_user']
+  mode '0755' # drwxr-xr-x
+end
+
+cookbook_file '/etc/systemd/system/capybara-webkit-daemon.service' do
+  source 'capybara-webkit-daemon.service'
+  owner 'root'
+  group 'root'
+  mode '0644' # -rw-r--r--
+
+  notifies :run, 'execute[systemctl daemon-reload]', :immediately
+end
+
+execute 'systemctl daemon-reload' do
+  action :nothing
+end
