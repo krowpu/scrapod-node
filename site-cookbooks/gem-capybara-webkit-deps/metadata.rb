dfc38e410c9fd5e606ea7362fd42686d754ec195
krowpu <krowpu@tightmail.com>
2017-03-12 17:13:23 +0500

    Rename cookbook "qt5-webkit-dev" to "gem-capybara-webkit-deps"

---
+++ site-cookbooks/gem-capybara-webkit-deps/metadata.rb
@@ -0,0 +1,6 @@
+name 'gem-capybara-webkit-deps'
+description 'Install dependencies for gem "capybara-webkit"'
+
+platform 'ubuntu', '= 16.04'
+
+depends 'dist-update'
--- site-cookbooks/qt5-webkit-dev/metadata.rb
+++
@@ -1,6 +0,0 @@
-name 'qt5-webkit-dev'
-description 'Install dependencies for Capybara Webkit'
-
-platform 'ubuntu', '= 16.04'
-
-depends 'dist-update'
