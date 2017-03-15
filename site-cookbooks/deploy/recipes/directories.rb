2d521cc099cd93f111978715f6dcdcda90a97555
krowpu <krowpu@tightmail.com>
2017-03-15 16:30:52 +0500

    Add directory

--- site-cookbooks/deploy/recipes/directories.rb
+++ site-cookbooks/deploy/recipes/directories.rb
@@ -10,6 +10,12 @@ directory '/srv/scrapod/shared' do
   mode '0755' # drwxr-xr-x
 end
 
+directory '/srv/scrapod/shared/log' do
+  owner node['deploy']['user']
+  group node['deploy']['group']
+  mode '0755' # drwxr-xr-x
+end
+
 directory '/srv/scrapod/shared/tmp' do
   owner node['deploy']['user']
   group node['deploy']['group']
