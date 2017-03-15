db48ad413fdfe59b95fde50817c48d25524cb9eb
krowpu <krowpu@tightmail.com>
2017-03-15 16:21:48 +0500

    Some refactoring

--- site-cookbooks/deploy/recipes/directories.rb
+++ site-cookbooks/deploy/recipes/directories.rb
@@ -1,23 +1,23 @@
 directory '/srv/scrapod' do
-  owner node['deploy_user']
-  group node['deploy_group']
+  owner node['deploy']['user']
+  group node['deploy']['group']
   mode '0755' # drwxr-xr-x
 end
 
 directory '/srv/scrapod/shared' do
-  owner node['deploy_user']
-  group node['deploy_user']
+  owner node['deploy']['user']
+  group node['deploy']['group']
   mode '0755' # drwxr-xr-x
 end
 
 directory '/srv/scrapod/shared/tmp' do
-  owner node['deploy_user']
-  group node['deploy_user']
+  owner node['deploy']['user']
+  group node['deploy']['group']
   mode '0755' # drwxr-xr-x
 end
 
 directory '/srv/scrapod/shared/tmp/pids' do
-  owner node['deploy_user']
-  group node['deploy_user']
+  owner node['deploy']['user']
+  group node['deploy']['group']
   mode '0755' # drwxr-xr-x
 end
