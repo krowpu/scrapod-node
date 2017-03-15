3fd3f7c5589cfe002c990402279fe5b36bd2be26
krowpu <krowpu@tightmail.com>
2017-03-15 16:18:41 +0500

    Some refactoring

--- site-cookbooks/service/recipes/default.rb
+++ site-cookbooks/service/recipes/default.rb
@@ -1,23 +1,5 @@
 include_recipe 'deploy'
 
-directory '/srv/scrapod/shared' do
-  owner node['deploy_user']
-  group node['deploy_user']
-  mode '0755' # drwxr-xr-x
-end
-
-directory '/srv/scrapod/shared/tmp' do
-  owner node['deploy_user']
-  group node['deploy_user']
-  mode '0755' # drwxr-xr-x
-end
-
-directory '/srv/scrapod/shared/tmp/pids' do
-  owner node['deploy_user']
-  group node['deploy_user']
-  mode '0755' # drwxr-xr-x
-end
-
 template '/etc/systemd/system/capybara-webkit-daemon.service' do
   source 'capybara-webkit-daemon.service.erb'
   owner 'root'
