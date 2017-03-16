6833e75d0e1f16e8919fdd0ebed2423707379523
krowpu <krowpu@tightmail.com>
2017-03-17 00:45:55 +0500

    Organize Monit config

--- site-cookbooks/monit/recipes/default.rb
+++ site-cookbooks/monit/recipes/default.rb
@@ -11,9 +11,42 @@ cookbook_file '/etc/monit/monitrc' do
   mode '0600' # -rw-------
 end
 
+cookbook_file '/etc/monit/conf-available/system' do
+  source 'system'
+  owner 'root'
+  group 'root'
+  mode '0644' # -rw-r--r--
+end
+
+link '/etc/monit/conf-enabled/system' do
+  to '/etc/monit/conf-available/system'
+  owner 'root'
+  group 'root'
+end
+
+cookbook_file '/etc/monit/conf-available/rootfs' do
+  source 'rootfs'
+  owner 'root'
+  group 'root'
+  mode '0644' # -rw-r--r--
+end
+
+link '/etc/monit/conf-enabled/rootfs' do
+  to '/etc/monit/conf-available/rootfs'
+  owner 'root'
+  group 'root'
+end
+
 service 'monit' do
   action :nothing
+
   subscribes :restart, 'cookbook_file[/etc/monit/monitrc]', :immediately
+
+  subscribes :restart, 'cookbook_file[/etc/monit/conf-available/system]', :immediately
+  subscribes :restart, 'cookbook_file[/etc/monit/conf-available/rootfs]', :immediately
+
+  subscribes :restart, 'cookbook_file[/etc/monit/conf-enabled/system]', :immediately
+  subscribes :restart, 'cookbook_file[/etc/monit/conf-enabled/rootfs]', :immediately
 end
 
 cookbook_file '/etc/nginx/snippets/monit.conf' do
