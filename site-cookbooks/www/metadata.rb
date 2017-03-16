4183bb33d065fefa2224a479eb22ad28fa405295
krowpu <krowpu@tightmail.com>
2017-03-16 23:29:54 +0500

    Create cookbook "www"

---
+++ site-cookbooks/www/metadata.rb
@@ -0,0 +1,9 @@
+name 'www'
+description 'Web server'
+license 'MIT'
+
+platform 'ubuntu', '= 16.04'
+
+depends 'deploy'
+depends 'nginx'
+depends 'monit'
---
+++ site-cookbooks/www/recipes/default.rb
@@ -0,0 +1,40 @@
+include_recipe 'nginx'
+include_recipe 'monit'
+
+directory '/srv/www' do
+  owner node['deploy']['user']
+  group node['deploy']['group']
+  mode '0755' # drwxr-xr-x
+end
+
+directory '/srv/www/html' do
+  owner node['deploy']['user']
+  group node['deploy']['group']
+  mode '0755' # drwxr-xr-x
+end
+
+cookbook_file '/etc/www/html/index.html' do
+  source 'index.html'
+  owner node['deploy']['user']
+  group node['deploy']['group']
+  mode '0644' # -rw-r--r--
+end
+
+cookbook_file '/etc/nginx/sites-available/default' do
+  source 'nginx.conf'
+  owner 'root'
+  group 'root'
+  mode '0644' # -rw-r--r--
+end
+
+link '/etc/nginx/sites-enabled/default' do
+  to '/etc/nginx/sites-available/default'
+  owner 'root'
+  group 'root'
+end
+
+service 'nginx' do
+  action :nothing
+  subscribes :restart, 'cookbook_file[/etc/nginx/sites-available/default]', :immediately
+  subscribes :restart, 'link[/etc/nginx/sites-enabled/default]',            :immediately
+end
