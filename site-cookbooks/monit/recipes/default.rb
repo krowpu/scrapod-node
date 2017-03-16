f0ffb31728d1189feecfdd8bbb2371dd57507b95
krowpu <krowpu@tightmail.com>
2017-03-17 00:07:49 +0500

    Some fix

--- site-cookbooks/monit/recipes/default.rb
+++ site-cookbooks/monit/recipes/default.rb
@@ -16,21 +16,14 @@ service 'monit' do
   subscribes :restart, 'cookbook_file[/etc/monit/monitrc]', :immediately
 end
 
-cookbook_file '/etc/nginx/sites-available/monit' do
+cookbook_file '/etc/nginx/snippets/monit.conf' do
   source 'nginx.conf'
   owner 'root'
   group 'root'
   mode '0644' # -rw-r--r--
 end
 
-link '/etc/nginx/sites-enabled/monit' do
-  to '/etc/nginx/sites-available/monit'
-  owner 'root'
-  group 'root'
-end
-
 service 'nginx' do
   action :nothing
-  subscribes :restart, 'cookbook_file[/etc/nginx/sites-available/monit]', :immediately
-  subscribes :restart, 'link[/etc/nginx/sites-enabled/monit]',            :immediately
+  subscribes :restart, 'cookbook_file[/etc/nginx/snippets/monit.conf]', :immediately
 end
