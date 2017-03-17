40f01c1bb351e39414783ac85071a1380b033b34
krowpu <krowpu@tightmail.com>
2017-03-17 16:03:41 +0500

    Rename Nginx www config

--- site-cookbooks/nginx/recipes/default.rb
+++ site-cookbooks/nginx/recipes/default.rb
@@ -1,3 +1,11 @@
 include_recipe 'dist-update'
 
 package 'nginx'
+
+cookbook_file '/etc/nginx/sites-available/default' do
+  action :delete
+end
+
+link '/etc/nginx/sites-enabled/default' do
+  action :delete
+end
--- site-cookbooks/scrapod/recipes/service_web.rb
+++ site-cookbooks/scrapod/recipes/service_web.rb
@@ -1,4 +1,6 @@
 include_recipe 'scrapod::directories'
+include_recipe 'nginx'
+include_recipe 'monit'
 
 template 'web.service' do
   path '/etc/systemd/system/scrapod-web.service'
@@ -18,21 +20,21 @@ execute 'systemctl daemon-reload' do
   subscribes :run, 'template[web.service]'
 end
 
-cookbook_file '/etc/nginx/sites-available/default' do
+cookbook_file '/etc/nginx/sites-available/www' do
   source 'nginx.conf'
   owner 'root'
   group 'root'
   mode '0644' # -rw-r--r--
 end
 
-link '/etc/nginx/sites-enabled/default' do
-  to '/etc/nginx/sites-available/default'
+link '/etc/nginx/sites-enabled/www' do
+  to '/etc/nginx/sites-available/www'
   owner 'root'
   group 'root'
 end
 
 service 'nginx' do
   action :nothing
-  subscribes :restart, 'cookbook_file[/etc/nginx/sites-available/default]'
-  subscribes :restart, 'link[/etc/nginx/sites-enabled/default]'
+  subscribes :restart, 'cookbook_file[/etc/nginx/sites-available/www]'
+  subscribes :restart, 'link[/etc/nginx/sites-enabled/www]'
 end
