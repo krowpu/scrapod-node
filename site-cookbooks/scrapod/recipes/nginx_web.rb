cb422c6ab80d7cbffb3366a221b02b4e02bb50d3
krowpu <krowpu@tightmail.com>
2017-03-17 16:22:21 +0500

    Some refactoring

--- site-cookbooks/scrapod/recipes/nginx_web.rb
+++ site-cookbooks/scrapod/recipes/nginx_web.rb
@@ -1,21 +1,21 @@
 include_recipe 'nginx'
 include_recipe 'monit'
 
-cookbook_file '/etc/nginx/sites-available/www' do
+cookbook_file '/etc/nginx/sites-available/web' do
   source 'nginx_web.conf'
   owner 'root'
   group 'root'
   mode '0644' # -rw-r--r--
 end
 
-link '/etc/nginx/sites-enabled/www' do
-  to '/etc/nginx/sites-available/www'
+link '/etc/nginx/sites-enabled/web' do
+  to '/etc/nginx/sites-available/web'
   owner 'root'
   group 'root'
 end
 
 service 'nginx' do
   action :nothing
-  subscribes :restart, 'cookbook_file[/etc/nginx/sites-available/www]'
-  subscribes :restart, 'link[/etc/nginx/sites-enabled/www]'
+  subscribes :restart, 'cookbook_file[/etc/nginx/sites-available/web]'
+  subscribes :restart, 'link[/etc/nginx/sites-enabled/web]'
 end
