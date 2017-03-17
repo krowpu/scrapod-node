4c8c6ea1267456cf3ac1bf4f7103bc0482ad9e89
krowpu <krowpu@tightmail.com>
2017-03-18 04:35:36 +0500

    Create directory for graphs

--- site-cookbooks/scrapod/recipes/directories.rb
+++ site-cookbooks/scrapod/recipes/directories.rb
@@ -33,3 +33,9 @@ directory '/srv/scrapod/shared/tmp/sockets' do
   group node['deploy']['group']
   mode '0755' # drwxr-xr-x
 end
+
+directory '/srv/scrapod/shared/tmp/graphs' do
+  owner node['deploy']['user']
+  group node['deploy']['group']
+  mode '0755' # drwxr-xr-x
+end
