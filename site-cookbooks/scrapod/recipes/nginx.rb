d40358e7f6ebae1b2d3585be2c9ca4b3ad27975e
krowpu <krowpu@tightmail.com>
2017-03-17 16:13:00 +0500

    Some refactoring

---
+++ site-cookbooks/scrapod/recipes/nginx.rb
@@ -0,0 +1,21 @@
+include_recipe 'nginx'
+include_recipe 'monit'
+
+cookbook_file '/etc/nginx/sites-available/www' do
+  source 'nginx.conf'
+  owner 'root'
+  group 'root'
+  mode '0644' # -rw-r--r--
+end
+
+link '/etc/nginx/sites-enabled/www' do
+  to '/etc/nginx/sites-available/www'
+  owner 'root'
+  group 'root'
+end
+
+service 'nginx' do
+  action :nothing
+  subscribes :restart, 'cookbook_file[/etc/nginx/sites-available/www]'
+  subscribes :restart, 'link[/etc/nginx/sites-enabled/www]'
+end
--- site-cookbooks/scrapod/recipes/service_web.rb
+++ site-cookbooks/scrapod/recipes/service_web.rb
@@ -1,6 +1,5 @@
 include_recipe 'scrapod::directories'
-include_recipe 'nginx'
-include_recipe 'monit'
+include_recipe 'scrapod::nginx'
 
 template 'web.service' do
   path '/etc/systemd/system/scrapod-web.service'
@@ -18,23 +17,4 @@ end
 execute 'systemctl daemon-reload' do
   action :nothing
   subscribes :run, 'template[web.service]'
-end
-
-cookbook_file '/etc/nginx/sites-available/www' do
-  source 'nginx.conf'
-  owner 'root'
-  group 'root'
-  mode '0644' # -rw-r--r--
-end
-
-link '/etc/nginx/sites-enabled/www' do
-  to '/etc/nginx/sites-available/www'
-  owner 'root'
-  group 'root'
-end
-
-service 'nginx' do
-  action :nothing
-  subscribes :restart, 'cookbook_file[/etc/nginx/sites-available/www]'
-  subscribes :restart, 'link[/etc/nginx/sites-enabled/www]'
 end
