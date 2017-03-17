f185fc90652e4150be73f53e8f9b0b37aff8b8d2
krowpu <krowpu@tightmail.com>
2017-03-18 00:30:45 +0500

    Add Monit services

---
+++ lib/scrapod/monit/status/filesystem.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'scrapod/monit/status/base'
+
+module Scrapod
+  module Monit
+    module Status
+      class Filesystem < Base
+      end
+    end
+  end
+end
---
+++ lib/scrapod/monit/status/process.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'scrapod/monit/status/base'
+
+module Scrapod
+  module Monit
+    module Status
+      class Process < Base
+      end
+    end
+  end
+end
