9a194969bfd15621c62b92995db3f5cfb3e85189
krowpu <krowpu@tightmail.com>
2017-03-18 02:43:39 +0500

    Delete service

--- site-cookbooks/scrapod/recipes/service_graphs.rb
+++ site-cookbooks/scrapod/recipes/service_graphs.rb
@@ -1,11 +1,6 @@
-include_recipe 'scrapod::directories'
-
 cookbook_file 'graphs.service' do
+  action :delete
   path '/etc/systemd/system/scrapod-graphs.service'
-  source 'graphs.service'
-  owner 'root'
-  group 'root'
-  mode '0644' # -rw-r--r--
 end
 
 execute 'systemctl daemon-reload' do
