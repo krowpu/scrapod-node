58259b430d5910e3e856cf6ef69301def07c5e3e
krowpu <krowpu@tightmail.com>
2017-03-18 06:56:07 +0500

    Display active sessions from Redis

--- web.rb
+++ web.rb
@@ -2,9 +2,19 @@
 
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'redis'
+
+redis = Redis.new
 
 get '/' do
-  erb :index
+  sessions = redis.smembers('sessions').map do |session_id|
+    {
+      id: session_id,
+      started_at: redis.get("session:#{session_id}:started_at"),
+    }
+  end
+
+  erb :index, locals: { sessions: sessions }
 end
 
 get '/monit-frame' do
