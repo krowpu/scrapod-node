7a99ed48153cecac8ad8d534623e806dde62415e
krowpu <krowpu@tightmail.com>
2017-03-18 06:50:25 +0500

    Run Capybara Webkit daemon with Redis

--- config/capybara_webkit_daemon/production.rb
+++ config/capybara_webkit_daemon/production.rb
@@ -5,3 +5,5 @@ self.log_level = :debug
 self.pid_file = '/srv/scrapod/shared/tmp/pids/capybara.pid'
 
 self.log_file = '/srv/scrapod/shared/log/capybara.log'
+
+self.redis_url = 'redis://127.0.0.1:6379'
