90c114f8cb0245a20b0f800d93312166e56a770b
krowpu <krowpu@tightmail.com>
2017-03-18 07:04:32 +0500

    Monit Redis

--- site-cookbooks/monit/recipes/default.rb
+++ site-cookbooks/monit/recipes/default.rb
@@ -98,6 +98,23 @@ link '/etc/monit/conf-enabled/nginx' do
   notifies :restart, 'service[monit]'
 end
 
+cookbook_file '/etc/monit/conf-available/redis-server' do
+  source 'redis-server'
+  owner 'root'
+  group 'root'
+  mode '0644' # -rw-r--r--
+
+  notifies :restart, 'service[monit]'
+end
+
+link '/etc/monit/conf-enabled/redis-server' do
+  to '/etc/monit/conf-available/redis-server'
+  owner 'root'
+  group 'root'
+
+  notifies :restart, 'service[monit]'
+end
+
 cookbook_file '/etc/monit/conf-available/scrapod-web' do
   source 'scrapod-web'
   owner 'root'
