6b44ecf52c607f729610a2d4eed622a84aee61d3
krowpu <krowpu@tightmail.com>
2017-03-19 15:26:17 +0500

    Use cookbook "redis" from external repository

--- site-cookbooks/redis/metadata.rb
+++
@@ -1,7 +0,0 @@
-name 'redis'
-description 'Redis server'
-license 'MIT'
-
-platform 'ubuntu', '= 16.04'
-
-depends 'dist-update'
--- site-cookbooks/redis/recipes/default.rb
+++
@@ -1,3 +0,0 @@
-include_recipe 'dist-update'
-
-package 'redis-server'
